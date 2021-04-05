997e7ce4820af3d5293e04177f3d0c63ef22af64
Irina <irinakir98@gmail.com>
2021-04-05 18:03:04 +0300

    web-worker in react with plagin workerize-loader

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import * as React from 'react';
+
+import worker from 'workerize-loader!./worker'; // eslint-disable-line import/no-webpack-loader-syntax
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const workerInstance = worker();
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: ''
+    };
+  }
 
-export default App;
+  handlerStartWorker = () => {
+    workerInstance.postMessage('bar');
+    workerInstance.addEventListener('message', (e) => {
+      this.setState({
+        result: e.data
+      });
+    });
+  };
+
+  handlerAlert = () => {
+    alert('Hello!');
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="wrap">
+          <div>
+            <button onClick={this.handlerStartWorker}>Start worker</button>
+            <div className="result">Result: {this.state.result}</div>
+          </div>
+          <button onClick={this.handlerAlert}>Show alert</button>
+        </div>
+      </div>
+    );
+  }
+}
